ab6f535204dfdaca16992cc587a47d0746068391
Uwe Fechner <u.fechner-1@tudelft.nl>
2025-07-01 18:20:52 +0200

    Make load_windfield() private

--- examples/load_windfield.jl
+++ examples/load_windfield.jl
@@ -4,6 +4,6 @@ set_data_path("data")
 set = load_settings("system.yaml")
 am = AtmosphericModel(set)
 
-x,y,z,u,v,w,param = load_windfield(am, 5.324)
+x,y,z,u,v,w,param = AtmosphericModels.load_windfield(am, 5.324)
 
 println("Windfield dimensions: $(size(x)), $(size(y)), $(size(z))")
