e102cc03f92b203ff861b3db72712feff44b4c93
Uwe Fechner <u.fechner-1@tudelft.nl>
2025-07-01 19:08:09 +0200

    Make create_windfield private

--- examples/create_windfield.jl
+++ examples/create_windfield.jl
@@ -10,7 +10,7 @@ x = range(0, 50, length=25)
 y = range(0, 800, length=400)
 z = range(0, 200, length=100)
 
-u, v, w = create_windfield(x, y, z; sigma1=1.2)
+u, v, w = AtmosphericModels.create_windfield(x, y, z; sigma1=1.2)
 am = AtmosphericModel(set)
 am.set.v_wind = v_wind_gnd
 AtmosphericModels.addWindSpeed(am, z, u)
