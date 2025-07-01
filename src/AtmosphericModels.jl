fc7156dcbac13f536138335697046fd313b8bacd
Uwe Fechner <u.fechner-1@tudelft.nl>
2025-07-01 12:15:21 +0200

    Use MeshGrid

--- src/AtmosphericModels.jl
+++ src/AtmosphericModels.jl
@@ -4,6 +4,7 @@ using KiteUtils
 using HypergeometricFunctions:_₂F₁
 using NPZ, Printf
 using FFTW, LinearAlgebra, Random, Statistics
+using MeshGrid
 
 export AtmosphericModel, ProfileLaw, EXP, LOG, EXPLOG, FAST_EXP, FAST_LOG, FAST_EXPLOG
 export clear, calc_rho, calc_wind_factor
