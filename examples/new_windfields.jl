aa4bc9b7e6ab450b4c37084fc33313816877f50d
Uwe Fechner <fechner@aenarete.eu>
2025-07-01 16:28:49 +0200

    Update examples

--- examples/new_windfields.jl
+++ examples/new_windfields.jl
@@ -4,6 +4,5 @@ set_data_path("data")
 set = load_settings("system.yaml")
 am = AtmosphericModel(set)
 
-x,y,z,u,v,w,param = load_windfield(am, 5.324)
-
-println("Windfield dimensions: $(size(x)), $(size(y)), $(size(z))")
+new_windfields(am)
+nothing
