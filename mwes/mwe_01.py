befe2977658302a8e25d7a9d388d079e84857b7c
Uwe Fechner <fechner@aenarete.eu>
2025-07-01 12:45:07 +0200

    Update mwe_01.py

--- mwes/mwe_01.py
+++ mwes/mwe_01.py
@@ -22,3 +22,22 @@ def create_grid(ny=50, nx=100, nz=50, z_min=25, res=GRID_STEP):
 
 y, x, z = create_grid(100, 4050, 500, 70)
 
+# Number of elements in each direction
+nx = x.shape[0]  #size(x,2);
+ny = y.shape[1]  #size(y,1);
+nz = z.shape[2]  #size(z,3);
+
+X, Y, Z = x, y, z
+
+# Wave number discretization
+# Shifted integers
+y_range=np.linspace(-ny/2., ny/2. - 1,  num=ny)
+x_range=np.linspace(-nx/2., nx/2. - 1,  num=nx)
+z_range=np.linspace(-nz/2., nz/2. - 1,  num=nz)
+m2, m1, m3 = np.meshgrid(y_range, x_range, z_range)
+print(m2.shape, " ", m1.shape, " ", m3.shape)
+
+m1 = np.fft.ifftshift(m1 + 1e-6)
+m2 = np.fft.ifftshift(m2 + 1e-6)
+m3 = np.fft.ifftshift(m3 + 1e-6)
+print(m2.shape, " ", m1.shape, " ", m3.shape)
