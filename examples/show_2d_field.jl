26eef89ab6c0d9b986538a3c3c59c2a42414ef4a
Uwe Fechner <u.fechner-1@tudelft.nl>
2025-07-02 12:16:45 +0200

    Some progress

--- examples/show_2d_field.jl
+++ examples/show_2d_field.jl
@@ -16,10 +16,16 @@ am = AtmosphericModel(set)
 
 x,y,z,u,v,w,param = AtmosphericModels.load_windfield(am, 5.324)
 
+x1 = x[:, 1, :][1:51,:]
+y1 = y[1, :, :]
+u1 = u[:, 1, :][1:51,:]
+v1 = v[1, :, :]
+
 X = -10:1:9
 Y = -10:1:9
 U, V = meshgrid(X, Y)
 
 fig, ax = plt.subplots()
-ax.quiver(X, Y, U, V)
+# ax.quiver(X, Y, U, V)
+ax.quiver(x1, y1, u1, v1)
 plt.show()
