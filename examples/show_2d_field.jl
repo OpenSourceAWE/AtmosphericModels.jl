483b72a4aee048b2ef9aaf69560a901ceec2f244
Uwe Fechner <u.fechner-1@tudelft.nl>
2025-07-02 12:04:01 +0200

    Improve example

--- examples/show_2d_field.jl
+++ examples/show_2d_field.jl
@@ -2,7 +2,7 @@ using Pkg
 if ! ("ControlPlots" ∈ keys(Pkg.project().dependencies))
     using TestEnv; TestEnv.activate()
 end
-using ControlPlots
+using ControlPlots, AtmosphericModels, KiteUtils
 
 function meshgrid(x, y)
     X = [i for i in x, _ in y]
@@ -10,12 +10,16 @@ function meshgrid(x, y)
     return (X, Y)
 end
 
+set_data_path("data") 
+set = load_settings("system.yaml")
+am = AtmosphericModel(set)
+
+x,y,z,u,v,w,param = AtmosphericModels.load_windfield(am, 5.324)
+
 X = -10:1:9
 Y = -10:1:9
 U, V = meshgrid(X, Y)
 
 fig, ax = plt.subplots()
-q = ax.quiver(X, Y, U, V)
-# ax.quiverkey(q, X=0.3, Y=1.1, U=10, label="Quiver key, length = 10", labelpos="E")
-
+ax.quiver(X, Y, U, V)
 plt.show()
