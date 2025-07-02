f4492ad24462ff5770454ed0a722e4b6c58f0720
Uwe Fechner <u.fechner-1@tudelft.nl>
2025-07-02 11:20:59 +0200

    Add quiver.jl

---
+++ examples/quiver.jl
@@ -0,0 +1,22 @@
+using Pkg
+if ! ("ControlPlots" ∈ keys(Pkg.project().dependencies))
+    using TestEnv; TestEnv.activate()
+end
+using ControlPlots
+
+function meshgrid(x, y)
+    X = [i for i in x, _ in y]
+    Y = [j for _ in x, j in y]
+    return (X, Y)
+end
+
+X = -10:1:9
+Y = -10:1:9
+U, V = meshgrid(X, Y)
+
+fig, ax = plt.subplots()
+q = ax.quiver(X, Y, U, V)
+ax.quiverkey(q, X=0.3, Y=1.1, U=10,
+             label="Quiver key, length = 10", labelpos="E")
+
+plt.show()
