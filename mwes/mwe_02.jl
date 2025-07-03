f0d7a39ffdec9aaee242daba1b5aaeb639f0f9b5
Uwe Fechner <u.fechner-1@tudelft.nl>
2025-07-03 15:39:59 +0200

    Update mwe

--- mwes/mwe_02.jl
+++ mwes/mwe_02.jl
@@ -1,4 +1,6 @@
-using BenchmarkTools
+using BenchmarkTools, OhMyThreads
 const A=rand(2026,51,251)
 @btime maximum(A)
-#   6.946 ms (0 allocations: 0 bytes)
+#   6.946 ms (0 allocations: 0 bytes) # 4.569 ms (0 allocations: 0 bytes)
+@btime treduce(max, A)
+# 3.589 ms (126 allocations: 9.86 KiB)
