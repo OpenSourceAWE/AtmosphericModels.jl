5314a66e558fed43a0811841199f7f1337508c30
Uwe Fechner <fechner@aenarete.eu>
2025-07-04 15:53:09 +0200

    Add menu.jl

---
+++ examples/menu.jl
@@ -0,0 +1,23 @@
+using REPL.TerminalMenus
+
+options = ["bench_get_wind = include(\"bench_get_wind.jl\")",
+           "plot_wind_vs_time_ = include(\"plot_wind_vs_time.jl\")",
+           "plot_windfield_ = include(\"plot_windfield.jl\")",
+           "quit"]
+
+function example_menu()
+    active = true
+    while active
+        menu = RadioMenu(options, pagesize=8)
+        choice = request("\nChoose function to execute or `q` to quit: ", menu)
+
+        if choice != -1 && choice != length(options)
+            eval(Meta.parse(options[choice]))
+        else
+            println("Left menu. Press <ctrl><d> to quit Julia!")
+            active = false
+        end
+    end
+end
+
+example_menu()
