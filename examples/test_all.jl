54a97a1106620eb326fe846875ffc3eedc4d9d7c
Uwe Fechner <fechner@aenarete.eu>
2025-07-05 17:34:14 +0200

    Tighter tolerances

--- examples/test_all.jl
+++ examples/test_all.jl
@@ -16,11 +16,13 @@ function analyze_windfield(am::AtmosphericModel; z=197.3)
     x = 0.0; y = 0.0
     v_wind_x = Float64[]
     v_wind_norm = Float64[]
-    for t in range(0.0, stop=600.0, length=601)
-        v_x, v_y, v_z = get_wind(am, x, y, z, t)
-        v_wind = sqrt(v_x^2 + v_y^2 + v_z^2)
-        push!(v_wind_norm, v_wind)
-        push!(v_wind_x, v_x)
+    for y in -50:5:50
+        for t in range(0.0, stop=600.0, length=601)
+            v_x, v_y, v_z = get_wind(am, x, y, z, t)
+            v_wind = sqrt(v_x^2 + v_y^2 + v_z^2)
+            push!(v_wind_norm, v_wind)
+            push!(v_wind_x, v_x)
+        end
     end
     su = std(v_wind_norm)
     v_mean = round(mean(v_wind_norm), digits=1)
@@ -35,7 +37,7 @@ for (i, v_wind_gnd) in pairs(am.set.v_wind_gnds)
     v_mean, ti = analyze_windfield(am)
     v_mean_99, ti_99 = analyze_windfield(am; z=99.0)
     @test ti_99 ≈ I99[i]  rtol=0.07
-    @test ti    ≈ I197[i] rtol=0.08
+    @test ti    ≈ I197[i] rtol=0.07
     @info "v_mean_99: $v_mean_99 m/s, ti_99: $ti_99 %"
     @info "v_mean_197: $v_mean m/s, ti_197: $ti %"
 end
