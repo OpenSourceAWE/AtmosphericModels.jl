f7102524a836b9966ed426a714f4e3ce6f28c9aa
Uwe Fechner <u.fechner-1@tudelft.nl>
2025-07-05 05:57:19 +0200

    Update bench.jl

--- test/bench.jl
+++ test/bench.jl
@@ -5,7 +5,7 @@ set = load_settings("system.yaml")
 am::AtmosphericModel = AtmosphericModel(set)
 
 am.set.profile_law=6
-# @benchmark calc_wind_factor(am, height, Val{profile_law}) setup=(height=Float64((6.0+rand()*500.0)))
 @benchmark calc_wind_factor(am, height) setup=(height=Float64((6.0+rand()*500.0)))
-# @benchmark calc_rho(am, height) setup=(height=Float64((6.0+rand()*500.0)))
 
+# 22ns for 3
+#  5ns for 6
