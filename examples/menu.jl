aab8da455d3626a050ee59af0861980dbf5e2ce0
Uwe Fechner <fechner@aenarete.eu>
2025-07-06 16:16:40 +0200

    Add plot wind shear near shore

--- examples/menu.jl
+++ examples/menu.jl
@@ -9,6 +9,7 @@ end
 options = ["bench_get_wind = include(\"bench_get_wind.jl\")",
            "get_wind_ = include(\"get_wind.jl\")",
            "load_windfield = include(\"load_windfield.jl\")",
+           "plot_windshear = include(\"plot_windshear.jl\")",
            "plot_wind_vs_time_ = include(\"plot_wind_vs_time.jl\")",
            "plot_windfield_ = include(\"plot_windfield.jl\")",
            "new_windfields_ = include(\"new_windfields.jl\")",
